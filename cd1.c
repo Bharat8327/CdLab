e12aa982cb5a1a1356333422a5acca61d5b1b701
Bharat8327 <bp609213@gmail.com>
2024-11-26 22:37:34 +0530

    labrecord

---
+++ cd1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+void main() {
+    FILE *fp;
+    char str[100];
+    char ch;
+    printf("opening the file in write mode \n");
+    fp = fopen("output.txt1", "w");
+    if (fp == NULL) {
+	printf("Error opening file for writing\n");
+    }
+        printf("Enter the data for the file \n");
+    fgets(str, sizeof(str), stdin);
+    fprintf(fp, "%s", str);
+    fclose(fp);
+    printf("opening file in read mode \n");
+    fp = fopen("output.txt", "r");
+     if (fp == NULL) {
+	printf("Error opening file for read mode\n");
+    }
+    while ((ch = getc(fp)) != EOF) {
+	putchar(ch);
+    }
+    fclose(fp);
+    getch();
+}
